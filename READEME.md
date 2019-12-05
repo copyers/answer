27940b1db9f9d3f33aeae90c3096ad4569456243
copyers <1206227301@qq.com>
2019-12-05 12:39:02 +0800

    cookies

--- READEME.md
+++ READEME.md
@@ -1,2 +1,19 @@
 ##问答社区
 git测试
+
+
+###脚本
+```sql
+create table user
+(
+    ID           int auto_increment
+        primary key,
+    ACCOUNT_ID   varchar(100) null,
+    NAME         varchar(50)  null,
+    TOKEN        char(36)     null,
+    GMT_CREATE   bigint       null,
+    GMT_MODIFIED bigint       null
+);
+
+```
+
--- src/main/java/com/answer/question/Controller/Answerquestion.java
+++ src/main/java/com/answer/question/Controller/Answerquestion.java
@@ -1,14 +1,34 @@
 package com.answer.question.Controller;
 
+import com.answer.question.mapper.UserMapper;
+import com.answer.question.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class Answerquestion {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie:cookies){
+            if (cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (user!=null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 
