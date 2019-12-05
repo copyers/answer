51bf49392f3243f998af3f570a7b2f2b6949359f
copyers <1206227301@qq.com>
2019-12-05 16:11:08 +0800

    publish function

--- src/main/java/com/answer/question/dto/AccessTokenDTO.java
+++ src/main/java/com/answer/question/dto/AccessTokenDTO.java
@@ -1,7 +1,9 @@
 package com.answer.question.dto;
 
+import lombok.Data;
 import netscape.security.PrivilegeTable;
 
+@Data
 public class AccessTokenDTO {
 
     private String client_id;
@@ -10,45 +12,6 @@ public class AccessTokenDTO {
     private String redirect_url;
     private String state;
 
-    public String getClient_id() {
-        return client_id;
-    }
-
-    public void setClient_id(String client_id) {
-        this.client_id = client_id;
-    }
-
-    public String getClient_secret() {
-        return client_secret;
-    }
-
-    public void setClient_secret(String client_secret) {
-        this.client_secret = client_secret;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getRedirect_url() {
-        return redirect_url;
-    }
-
-    public void setRedirect_url(String redirect_url) {
-        this.redirect_url = redirect_url;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
 
     @Override
     public String toString() {
