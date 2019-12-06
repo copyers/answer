52d7d5a478b8d31a3335a26a0a583427e187b115
copyers <1206227301@qq.com>
2019-12-06 13:33:20 +0800

    pagehelper

--- src/main/java/com/answer/question/Controller/Answerquestion.java
+++ src/main/java/com/answer/question/Controller/Answerquestion.java
@@ -1,19 +1,26 @@
 package com.answer.question.Controller;
 
+import com.answer.question.dto.PaginationDTO;
 import com.answer.question.dto.QuestionDTO;
 import com.answer.question.mapper.QuestionMapper;
 import com.answer.question.mapper.UserMapper;
 import com.answer.question.model.Question;
 import com.answer.question.model.User;
 import com.answer.question.service.QuestionService;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.naming.Name;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -23,10 +30,15 @@ public class Answerquestion {
     private UserMapper userMapper;
 
     @Autowired
+    private QuestionMapper questionMapper;
+
+    @Autowired
     private QuestionService questionService;
 
     @GetMapping("/")
-    public String index(HttpServletRequest request, Model model){
+    public String index(HttpServletRequest request, Model model,
+                        @RequestParam(name = "page",defaultValue = "1")Integer page,
+                        @RequestParam(name = "size",defaultValue = "2")Integer size){
         Cookie[] cookies = request.getCookies();
         if (cookies != null && cookies.length != 0)
             for (Cookie cookie:cookies){
@@ -41,9 +53,18 @@ public class Answerquestion {
             }
 
 
-        List<QuestionDTO> questionList = questionService.list();
-        model.addAttribute("questions",questionList);
+        PaginationDTO pagination = questionService.list(page,size);
+        model.addAttribute("pagination",pagination);
         return "index";
     }
+
+
+    @ResponseBody
+    @GetMapping("/test/{id}")
+    public List<QuestionDTO> test(@PathVariable("id") Integer pagenum){
+        PageHelper.startPage(pagenum,3);
+        return questionMapper.selectbypagehelper();
+    }
+
 
 }
--- src/main/java/com/answer/question/service/QuestionService.java
+++ src/main/java/com/answer/question/service/QuestionService.java
@@ -1,5 +1,6 @@
 package com.answer.question.service;
 
+import com.answer.question.dto.PaginationDTO;
 import com.answer.question.dto.QuestionDTO;
 import com.answer.question.mapper.QuestionMapper;
 import com.answer.question.mapper.UserMapper;
@@ -22,9 +23,24 @@ public class QuestionService {
     private UserMapper userMapper;
 
 
-    public List<QuestionDTO> list(){
-        List<Question> questions = questionMapper.list();
+    public PaginationDTO list(Integer page, Integer size){
+
+        Integer offset = size * (page - 1);
+
+        List<Question> questions = questionMapper.list(offset,size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
+        PaginationDTO paginationDTO = new PaginationDTO();
+
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount,page,size);
+        if (page<1){
+            page = 1;
+        }
+        if (page>paginationDTO.getTotalPage()){
+            page = paginationDTO.getTotalPage();
+        }
+
+
         for (Question question:questions){
             User user = userMapper.findById(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
@@ -32,7 +48,7 @@ public class QuestionService {
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
         }
-
-        return questionDTOList;
+        paginationDTO.setQuestions(questionDTOList);
+        return paginationDTO;
     }
 }
